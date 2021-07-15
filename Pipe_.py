b1263efdc667e23a746211f64174316fcf172cbb
namiq <namiqefendiyev@gmail.com>
2021-07-15 13:46:19 +0400

    xeriteler ekrandan silinir

--- Pipe_.py
+++ Pipe_.py
@@ -2,21 +2,19 @@ from tkinter import Tk
 from tkinter.tix import Menu
 from tkinter import Button
 from tkinter import PhotoImage
-
-
-
+from PIL import ImageTk, Image
 # ------------------------------------------------------FORM 1 ÜÇÜN PARAMETRLƏR
 root = Tk()
 root.geometry("1920x1080")
 root.wm_attributes("-topmost", 1)                                            # Həmişə bütün pəncərələrdən üstdə olur
 root.title("Расчет и конструирование водопропускных труб")
 root.iconname("c:\clone\icon\App.ico")
-
-
+#------------------------------------------------------- Meny Bar
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Создать")
+
 filemenu.add_command(label="Oткрыть")
 filemenu.add_command(label="Сохранить")
 filemenu.add_command(label="Сохранить как ...")
@@ -52,8 +50,7 @@ helpmenu.add_command(label="About...")
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
-
-##----------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------Button 7 ve 8
 photo = PhotoImage(file = r"c:\clone\icon\Sozdat_file.png")
 photoimage1 = photo.subsample(1, 1)
 But1 = Button(root, width=20, height=20, image=photoimage1, relief='raised', borderwidth=1)
@@ -83,6 +80,23 @@ photo = PhotoImage(file=r"c:\clone\icon\Rascet_ico.png")
 photoimage6 = photo.subsample(2, 2)
 But6 = Button(root, width=20, height=20, image=photoimage6, relief='raised', borderwidth=1)
 But6.place(x=185, y=0)
+
+#------------------------------------------------------------------------------------------------------------
+
+def click_button_Az():
+    But7.destroy()
+def click_button_Rus():
+    But8.destroy()
+
+photo = PhotoImage(file=r"c:\clone\icon\Rus.gif")
+Az = photo.subsample(1, 1)
+But7 = Button(root, width=890, height=550, image=Az, relief='raised', borderwidth=1, command=click_button_Az)
+But7.place(x=470, y=100)
+
+photo = PhotoImage(file=r"c:\clone\icon\Az.gif")
+Rus = photo.subsample(1, 1)
+But8 = Button(root, width=890, height=550, image=Rus, relief='raised', borderwidth=1, command=click_button_Rus)
+But8.place(x=470, y=100)
 
 
 
